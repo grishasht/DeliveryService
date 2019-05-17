9e2fc95f4226c2b82bc4503697d5d4dfd3c72168
Heorhii <grishasht@bigmir.net>
2019-05-17 16:44:04 +0300

    Entities created

--- src/main/java/model/entity/Address.java
+++ src/main/java/model/entity/Address.java
@@ -1,4 +1,79 @@
 package model.entity;
 
+import java.util.Objects;
+
 public class Address {
+    private Integer id;
+    private String country;
+    private String city;
+    private String street;
+    private Integer house;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getHouse() {
+        return house;
+    }
+
+    public void setHouse(Integer house) {
+        this.house = house;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id) &&
+                Objects.equals(country, address.country) &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(street, address.street) &&
+                Objects.equals(house, address.house);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, country, city, street, house);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", house=" + house +
+                '}';
+    }
 }
