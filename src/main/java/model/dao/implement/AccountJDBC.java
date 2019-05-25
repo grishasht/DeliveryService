5c8a54dd542fd91bf2c357bfd05e218040bd7e47
Heorhii <grishasht@bigmir.net>
2019-05-25 14:35:28 +0300

    try -> try-with-resources

--- src/main/java/model/dao/implement/AccountJDBC.java
+++ src/main/java/model/dao/implement/AccountJDBC.java
@@ -5,7 +5,6 @@ import model.dao.mapper.AccountMapper;
 import model.entity.Account;
 import model.util.LogGenerator;
 import org.apache.log4j.Logger;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
@@ -35,29 +34,23 @@ public class AccountJDBC implements AccountDao {
 
     @Override
     public void create(Account entity) {
-        PreparedStatement preparedStatement = null;
-        String query = "INSERT INTO accounts(APPLICATION_ID, AMOUNT, DATE, IS_PAID)" +
+        final String QUERY = "INSERT INTO accounts(APPLICATION_ID, AMOUNT, DATE, IS_PAID)" +
                 "VALUES (?, ?, ?, ?)";
 
-        try {
-            preparedStatement = connection.prepareStatement(query);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(QUERY)){
             log.debug(properties.getProperty("PREP_STAT_OPEN") + "in AccountJDBC create");
+
             preparedStatement.setInt(1, entity.getApplicationId());
             preparedStatement.setInt(2, entity.getAmount());
             preparedStatement.setDate(3, entity.getDate());
             preparedStatement.setBoolean(4, entity.getPaid());
-            preparedStatement.execute(query);
+
+            preparedStatement.execute(QUERY);
             log.debug(properties.getProperty("SUCCESS_QUERY_EXECUTE") + "in AccountJDBC create");
+            log.debug(properties.getProperty("PREP_STAT_CLOSE") + "in AccountJDBC create");
+
         } catch (SQLException e) {
             log.error(properties.getProperty("SQL_EXC_WHILE_CREATE") + "in AccountJDBC");
-        } finally {
-            try {
-                if (preparedStatement != null)
-                    preparedStatement.close();
-                log.debug(properties.getProperty("PREP_STAT_CLOSE") + "in AccountJDBC create");
-            } catch (SQLException e) {
-                log.error(properties.getProperty("SQL_EXC_WHILE_CLOSE_PREP") + "in AccountJDBC create");
-            }
         }
     }
 
@@ -69,41 +62,25 @@ public class AccountJDBC implements AccountDao {
     @Override
     public List<Account> readAll() {
         List<Account> accounts = new LinkedList<>();
-        String query = "SELECT * FROM addresses";
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
+        final String QUERY = "SELECT * FROM addresses";
         AccountMapper accountMapper = new AccountMapper();
 
-        try {
-            preparedStatement = connection.prepareStatement(query);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(QUERY)){
             log.debug(properties.getProperty("PREP_STAT_OPEN") + "in AccountJDBC readAll");
-            resultSet = preparedStatement.executeQuery();
-            log.debug(properties.getProperty("RES_SET_OPEN") + "in AccountJDBC readAll");
 
-            while (resultSet.next()) {
-                accounts.add(accountMapper.getEntityFromResSet(resultSet, 1, 2, 3, 4, 5));
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                log.debug(properties.getProperty("RES_SET_OPEN") + "in AccountJDBC readAll");
+
+                while (resultSet.next()) {
+                    accounts.add(accountMapper.getEntityFromResSet(resultSet, 1, 2, 3, 4, 5));
+                }
+                log.debug(properties.getProperty("RES_SET_CLOSE") + "in AccountJDBC");
             }
+            log.debug(properties.getProperty("PREP_STAT_CLOSE") + "in AccountJDBC readAll");
+
         } catch (SQLException e) {
             log.error(properties.getProperty("SQL_EXC_WHILE_READ") + "in AccountJDBC");
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                    log.debug(properties.getProperty("RES_SET_CLOSE") + "in AccountJDBC");
-                }
-                try {
-                    if (preparedStatement != null) {
-                        preparedStatement.close();
-                        log.debug(properties.getProperty("PREP_STAT_CLOSE") + "in AccountJDBC readAll");
-                    }
-                } catch (SQLException e) {
-                    log.error(properties.getProperty("SQL_EXC_WHILE_CLOSE_PREP") + "in AccountJDBC readAll");
-                }
-            } catch (SQLException e) {
-                log.error(properties.getProperty("SQL_EXC_WHILE_CLOSE_RES_SET") + "in AccountJDBC readAll");
-            }
         }
-
 
         return accounts;
     }
@@ -115,24 +92,18 @@ public class AccountJDBC implements AccountDao {
 
     @Override
     public void delete(Integer id) {
-        String query = "DELETE FROM accounts WHERE id = " + id;
-        PreparedStatement preparedStatement = null;
+        final String QUERY = "DELETE FROM accounts WHERE id = " + id;
 
-        try {
-            preparedStatement = connection.prepareStatement(query);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(QUERY)){
             log.debug(properties.getProperty("PREP_STAT_OPEN") + "in AccountJDBC deleting");
             preparedStatement.execute();
             log.debug(properties.getProperty("SUCCESS_QUERY_EXECUTE") + "in AccountJDBC deleting");
+            log.debug(properties.getProperty("PREP_STAT_CLOSE") + "in AccountJDBC deleting");
+
         } catch (SQLException e) {
             log.error(properties.getProperty("SQL_EXC_WHILE_DELETE") + "in AccountJDBC");
-        } finally {
-            try {
-                if (preparedStatement != null)
-                    preparedStatement.close();
-            } catch (SQLException e) {
-                log.error(properties.getProperty("SQL_EXC_WHILE_CLOSE_PREP") + "in AccountJDBC deleting");
-            }
         }
+
     }
 
     @Override
