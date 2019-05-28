040d82460455f5858b69af8e65f4af0545c2c969
Heorhii <grishasht@bigmir.net>
2019-05-28 22:46:50 +0300

    Language filter added

---
+++ src/main/java/controller/filter/LanguageFilter.java
@@ -0,0 +1,32 @@
+package controller.filter;
+
+import model.util.Constants;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Locale;
+
+public class LanguageFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Locale.setDefault(Locale.ENGLISH);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        if (request.getParameter(Constants.CUR_LANG) == null){
+            request.getSession().setAttribute(Constants.CUR_LANG, request.getParameter(Constants.CUR_LANG));
+        } else {
+            request.getSession().setAttribute(Constants.CUR_LANG, Locale.getDefault().getLanguage());
+        }
+
+        chain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
